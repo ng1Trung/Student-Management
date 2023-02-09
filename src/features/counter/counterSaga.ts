c1c0205a3efabd3f876f8ddb456b55b2a129e06a
jasontr <ngtrung4520@gmail.com>
2023-02-09 18:10:03 +0700

    Setup axios client + API

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,12 +1,29 @@
 import { PayloadAction } from '@reduxjs/toolkit'
-import { takeEvery } from 'redux-saga/effects'
+import { takeEvery, takeLatest, delay, put } from 'redux-saga/effects'
+import { incrementSaga, incrementSagaSuccess } from './counterSlice'
 
-export function* log(action: PayloadAction) {
-    console.log('Log', action)
+// export function* log(action: PayloadAction) {
+//     console.log('Log', action)
+//     // console.log(incrementSaga.toString())
+// }
+
+function* handleIncrementSaga(action: PayloadAction<number>) {
+    console.log('handle increment saga')
+    // console.log(action.type)
+
+    //wait 2s
+    console.log('wait 2s')
+
+    yield delay(2000)
+
+    console.log('wait done then dispatch')
+
+    //dispatch
+    yield put(incrementSagaSuccess(action.payload))
 }
 
 export default function* counterSaga() {
     console.log('Counter Saga')
 
-    yield takeEvery('*', log)
+    yield takeEvery(incrementSaga.toString(), handleIncrementSaga)
 }
